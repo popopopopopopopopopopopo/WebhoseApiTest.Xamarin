869da918b3eb7a232a33750ad46fda3252965011
kosun <temparamental020@gmail.com>
2018-05-15 17:53:07 +0900

    Xamarinを最新の3.0.0.～に更新

---
+++ WebhoseApiTest.Xamarin/WebhoseApiTest.iOS/Implementations/TextToSpeakImplementation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebhoseApiTest.Interfaces;
+using Foundation;
+using UIKit;
+using AVFoundation;
+using Xamarin.Forms;
+using WebhoseApiTest.iOS.Implementations;
+
+[assembly: Dependency(typeof(TextToSpeakImplementation))]
+namespace WebhoseApiTest.iOS.Implementations
+{
+    public class TextToSpeakImplementation : ITextToSpeak
+    {
+        public TextToSpeakImplementation() { }
+
+        public void Speak(string text)
+        {
+            var speechSynthesizer = new AVSpeechSynthesizer();
+
+        }
+    }
+}
